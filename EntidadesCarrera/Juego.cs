645741528463d8d440c7e4893cedd266d9580809
Ansel Corona <AnselCorona@gmail.com>
2019-01-18 12:34:01 -0400

    Modified Juego.cs

--- EntidadesCarrera/Juego.cs
+++ EntidadesCarrera/Juego.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace EntidadesCarrera
 {
@@ -12,6 +13,8 @@ namespace EntidadesCarrera
 
         public Carrera Carrera { get; set; }
         public eEstadoJuego Estado { get; set; }
+        int _opcionMenu = -1;
+        Thread _procesoCaptura;
         #endregion
 
         #region "Enums"
@@ -19,6 +22,7 @@ namespace EntidadesCarrera
         {
             Iniciado,
             EnProceso,
+            Apostando,
             Terminado
         }
         #endregion
@@ -26,11 +30,28 @@ namespace EntidadesCarrera
         #region "Comportamientos"
         public void Iniciar()
         {
-
+            Estado = eEstadoJuego.Iniciado;
+            _procesoCaptura = new Thread(CapturarTeclado);
+            _procesoCaptura.Start();
         }
+
 
         public void Actualizar()
         {
+            switch (Estado)
+            {
+                case eEstadoJuego.Iniciado:
+                    if (_opcionMenu != -1 && 
+                    _opcionMenu == 1)
+                    {
+                        Estado = eEstadoJuego.Apostando;
+                    }
+                    else if(_opcionMenu!=-1 && _opcionMenu == 0)
+                    {
+                        Estado = eEstadoJuego.Terminado;
+                    }
+                    break;
+            }
 
         }
 
@@ -44,8 +65,9 @@ namespace EntidadesCarrera
                     break;
             }
         }
-        public void Finalizar() { 
-        
+        public void Finalizar() {
+            _procesoCaptura.Abort();
+            _procesoCaptura.Join();
         }
 
         private void MostrarMenu()
@@ -55,6 +77,19 @@ namespace EntidadesCarrera
             Console.WriteLine("1: Iniciar partida");
             Console.WriteLine("0: Salir");
         }
+        public void CapturarTeclado()
+        {
+            switch (Estado)
+            {
+                case eEstadoJuego.Iniciado:
+                    Int32.TryParse(Console.ReadKey().KeyChar.ToString(), out _opcionMenu);
+
+                    if (_opcionMenu < 0 || _opcionMenu > 1)
+                        _opcionMenu = -1;
+                    break;
+            }
+        }
+
         #endregion
     }
 }
