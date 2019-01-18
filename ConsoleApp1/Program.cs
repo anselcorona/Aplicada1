6dbc6e81692aaf51d49321cb19273c8f2307329c
Ansel Corona <AnselCorona@gmail.com>
2019-01-18 12:13:29 -0400

    all files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using EntidadesCarrera;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static Juego Juego { get; set; }
+
+        static void Main(string[] args)
+        {
+            Juego = new Juego();
+            Juego.Iniciar();
+
+            while(Juego.Estado!= Juego.eEstadoJuego.Terminado) {
+                Juego.Actualizar();
+                Juego.Dibujar();
+                Thread.Sleep(42);
+            }
+
+            Juego.Finalizar();
+        }
+    }
+}
---
+++ EntidadesCarrera/Juego.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesCarrera
+{
+    public class Juego
+    {
+        #region "Atributos"
+
+        public Carrera Carrera { get; set; }
+        public eEstadoJuego Estado { get; set; }
+        #endregion
+
+        #region "Enums"
+        public enum eEstadoJuego
+        {
+            Iniciado,
+            EnProceso,
+            Terminado
+        }
+        #endregion
+
+        #region "Comportamientos"
+        public void Iniciar()
+        {
+
+        }
+
+        public void Actualizar()
+        {
+
+        }
+
+        public void Dibujar()
+        {
+            Console.Clear();
+            switch (Estado)
+            {
+                case eEstadoJuego.Iniciado:
+                    MostrarMenu();
+                    break;
+            }
+        }
+        public void Finalizar() { 
+        
+        }
+
+        private void MostrarMenu()
+        {
+            Console.WriteLine("Bienvenido a Carrera de Caballos.");
+            Console.WriteLine("Seleccione una opcion:");
+            Console.WriteLine("1: Iniciar partida");
+            Console.WriteLine("0: Salir");
+        }
+        #endregion
+    }
+}
